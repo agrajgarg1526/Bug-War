cba20e1bed7c6a768e5461e5f2e2ce637cd40ac6
Agraj <agrajgarg1509@gmail.com>
2021-02-11 01:55:26 +0530

    Add Sign in and Sign up Page

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
+app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-   res.sendFile(__dirname+"/index.html");
-  });
+  res.render("index");
+});
 
-  
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.get("/signup", function (req, res) {
+  res.render("signup");
+});
+
+
 app.listen(3000, function () {
-    console.log("Server Running at 3000 port");
-  });
-  
+  console.log("Server Running at 3000 port");
+});
